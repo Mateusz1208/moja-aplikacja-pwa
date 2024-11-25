c96686f53426f10d1c5b99463c8530d9f89ebeb8
Mateusz Oskwarek <mateuszo1618@gmail.com>
2024-11-25 17:24:25 +0100

    Poprawiono ścieżki w Service Worker

--- sw.js
+++ sw.js
@@ -1,41 +1,20 @@
-// sw.js
-
-
-
 self.addEventListener('install', function(event) {
-
     event.waitUntil(
-
         caches.open('my-cache').then(function(cache) {
-
             return cache.addAll([
-
-                '/',
-
-                '/index.html',
-
-                '/script.js',
-
-                '/style.css'
-
+                '/moja-aplikacja-pwa/', 
+                '/moja-aplikacja-pwa/index.html', 
+                '/moja-aplikacja-pwa/script.js', 
+                '/moja-aplikacja-pwa/style.css' 
             ]);
-
         })
-
     );
-
 });
 
 self.addEventListener('fetch', function(event) {
-
     event.respondWith(
-
         caches.match(event.request).then(function(response) {
-
             return response || fetch(event.request);
-
         })
-
     );
-
 });
